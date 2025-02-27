80d0d39dcd3ee89a6bf864b0ff9e18a50b0cebb6
bowook <mbosx316@naver.com>
2025-02-27 13:55:22 +0900

    refactor(index.js) : 빈 값 분기처리

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 function getInput() {
   const typped = document.getElementById("input1").value;
+  if (typped == '') {
+    alert("댓글을 입력하셔야 합니다.");
+    return;
+  }
   alert("댓글이 등록되었습니다");
 
   const ul = document.querySelector(".comment-list");
